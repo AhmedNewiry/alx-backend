d95a8b5224a10d06940358a961b909fa79a970f1
ahmedNewiry <ahmedabadalfttah@outlook.com>
2024-08-27 23:23:11 +0000

    fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """
@@ -16,6 +17,7 @@ def index():
         A rendered HTML template with a welcome message.
     """
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
