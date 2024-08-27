be01f1875f110d037ae391ed3eb75abe91363543
ahmedNewiry <ahmedabadalfttah@outlook.com>
2024-08-27 23:18:09 +0000

    app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+0-app.py: A basic Flask application with a single route.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """
+    Renders the index.html template.
+
+    Returns:
+        A rendered HTML template with a welcome message.
+    """
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
