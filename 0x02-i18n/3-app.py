e913f16ce1facfc6f629eac46ca018a6f78f5d95
ahmedNewiry <ahmedabadalfttah@outlook.com>
2024-08-28 00:32:28 +0000

    fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -6,25 +6,30 @@ with English and French translations.
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
+
 class Config:
     """Configuration for Babel and supported languages."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
     """Determine the best match with our supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """Render the index page with localized messages."""
     return render_template('3-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
