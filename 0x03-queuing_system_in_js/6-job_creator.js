d076c803118a2c9b3be050eb9f57f9d86dfe0d2f
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-09-24 07:01:46 +0000

    job processor

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,25 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'This is the notification message',
+};
+
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    } else {
+      console.error('Failed to create the job:', err);
+    }
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
